06cd5fd2b3d1a21e7bc4d022e6a77b87312ef2a3
saishankar2748 <saishankar2748@gmail.com>
2022-10-15 21:14:06 +0530

    madlibs game

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# String concatination
+# youtuber = 'linkedin'
+
+# print('Follow me on'+youtuber)
+# print('Follow me on{}'.format(youtuber))
+# print(f'Follow me on{youtuber}')
+
+adj = input('adjective:')
+verb1 = input('verb:')
+verb2 = input('verb:')
+famous_person = input('famous person:')
+
+madlib = f"Computer Programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}.Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
